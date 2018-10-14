3c670e6a215ce692d355162747ed5b832b2b3fe7
adminder <123@123>
2018-10-14 19:13:05 +0800

    idel第一次上传

---
+++ idel_updown_git/src/com/itheima/domain/Testgit.java
@@ -0,0 +1,10 @@
+package com.itheima.domain;
+
+public class Testgit {
+    public static void main(String[] args) {
+        System.out.println("爱生活,爱java");
+        System.out.println("爱生活,爱java");
+        System.out.println("爱生活,爱java");
+        System.out.println("爱生活,爱java");
+    }
+}
